bd683d97d530b4439125af69a42980f1eba6343b
Mafe <ferdlgpar@gmail.com>
2023-09-04 20:12:16 -0500

    eliminacion apps

--- Back/nexosProject/departamentosapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return render(request,'index.html',{
-        
-    }) #la funcion recibe 3 parametros la peticion, el documento y un diccionario 
-# Create your views here.
-
