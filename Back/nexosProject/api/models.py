2fc4a229bfa82514208b668e42d7cf69d78161df
Mafe <ferdlgpar@gmail.com>
2023-09-06 07:42:51 -0500

    crud api, tabla ciudades

--- Back/nexosProject/api/models.py
+++ Back/nexosProject/api/models.py
@@ -2,31 +2,40 @@ from django.db import models
 
 # Create your models here.
 
+class Ciudades(models.Model):
+    id_ciudad = models.AutoField(primary_key=True)
+    id_departamento_fk = models.ForeignKey('Departamentos', models.DO_NOTHING, db_column='id_departamento_fk', blank=True, null=True)
+    nombre_ciudad = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ciudades'
+
+
 class Departamentos(models.Model):
     id_departamento = models.AutoField(primary_key=True)
     codigo_departamento = models.IntegerField(blank=True, null=True)
     nombre_departamento = models.CharField(max_length=30, blank=True, null=True)
-    fecha_hora_crear = models.DateTimeField(blank=True, null=True)
-    fecha_hora_modificar = models.DateTimeField(blank=True, null=True)
+    fecha_hora_crear = models.DateTimeField(auto_now_add=True)
+    fecha_hora_modificar = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         managed = False
         db_table = 'departamentos'
 
-        
 class Empleados(models.Model):
     id_empleado = models.AutoField(primary_key=True)
-    tipo_documento_fk = models.ForeignKey('TipoDocumento', models.DO_NOTHING, db_column='tipo_documento_fk', blank=True, null=True)
+    tipo_documento_fk = models.ForeignKey('TipoDocumento', models.DO_NOTHING, db_column='tipo_documento_fk')
     numero_documento = models.IntegerField(blank=True, null=True)
     nombres_empleado = models.CharField(max_length=30, blank=True, null=True)
     apellidos_empleado = models.CharField(max_length=50, blank=True, null=True)
-    id_departamento_fk = models.ForeignKey(Departamentos, models.DO_NOTHING, db_column='id_departamento_fk', blank=True, null=True)
-    ciudad = models.CharField(max_length=20, blank=True, null=True)
+    id_departamento_fk = models.ForeignKey(Departamentos, models.DO_NOTHING, db_column='id_departamento_fk')
+    id_ciudad_fk = models.ForeignKey(Ciudades, models.DO_NOTHING, db_column='id_ciudad_fk', blank=True, null=True)
     direccion = models.CharField(max_length=20, blank=True, null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
     telefono = models.IntegerField(blank=True, null=True)
-    fecha_hora_crear = models.DateTimeField(blank=True, null=True)
-    fecha_hora_modificar = models.DateTimeField(blank=True, null=True)
+    fecha_hora_crear = models.DateTimeField(auto_now_add=True)
+    fecha_hora_modificar = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         managed = False
@@ -39,3 +48,4 @@ class TipoDocumento(models.Model):
     class Meta:
         managed = False
         db_table = 'tipo_documento'
+
--- Back/nexosProject/models.py
+++ Back/nexosProject/models.py
@@ -8,31 +8,155 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Ciudades(models.Model):
+    id_ciudad = models.AutoField(primary_key=True)
+    id_departamento_fk = models.ForeignKey('Departamentos', models.DO_NOTHING, db_column='id_departamento_fk', blank=True, null=True)
+    nombre_ciudad = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ciudades'
+
+
 class Departamentos(models.Model):
     id_departamento = models.AutoField(primary_key=True)
     codigo_departamento = models.IntegerField(blank=True, null=True)
     nombre_departamento = models.CharField(max_length=30, blank=True, null=True)
-    fecha_hora_crear = models.DateTimeField(blank=True, null=True)
-    fecha_hora_modificar = models.DateTimeField(blank=True, null=True)
+    fecha_hora_crear = models.DateTimeField()
+    fecha_hora_modificar = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'departamentos'
 
 
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Empleados(models.Model):
     id_empleado = models.AutoField(primary_key=True)
-    tipo_documento_fk = models.ForeignKey('TipoDocumento', models.DO_NOTHING, db_column='tipo_documento_fk', blank=True, null=True)
+    tipo_documento_fk = models.ForeignKey('TipoDocumento', models.DO_NOTHING, db_column='tipo_documento_fk')
     numero_documento = models.IntegerField(blank=True, null=True)
     nombres_empleado = models.CharField(max_length=30, blank=True, null=True)
     apellidos_empleado = models.CharField(max_length=50, blank=True, null=True)
-    id_departamento_fk = models.ForeignKey(Departamentos, models.DO_NOTHING, db_column='id_departamento_fk', blank=True, null=True)
-    ciudad = models.CharField(max_length=20, blank=True, null=True)
+    id_departamento_fk = models.ForeignKey(Departamentos, models.DO_NOTHING, db_column='id_departamento_fk')
+    id_ciudad_fk = models.ForeignKey(Ciudades, models.DO_NOTHING, db_column='id_ciudad_fk', blank=True, null=True)
     direccion = models.CharField(max_length=20, blank=True, null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
     telefono = models.IntegerField(blank=True, null=True)
-    fecha_hora_crear = models.DateTimeField(blank=True, null=True)
-    fecha_hora_modificar = models.DateTimeField(blank=True, null=True)
+    fecha_hora_crear = models.DateTimeField()
+    fecha_hora_modificar = models.DateTimeField()
 
     class Meta:
         managed = False
