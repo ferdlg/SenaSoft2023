4c21fe88572492eb7778984cf57d8c0ddea3e3b6
Camila Arias <ariasruizcamilaa@gmail.com>
2023-09-13 15:50:25 -0500

    Buttons TableEmployees

--- front/src/services/EmployeeService.js
+++ front/src/services/EmployeeService.js
@@ -3,10 +3,17 @@ import axios from 'axios';
 const EmployeeService = {
   getDepartments: async () => {
     try {
-      const response = await axios.get('https://api-colombia.com/api/v1/Department');
-      const data = response.data;
-      const departamentosConId = data.map((dep) => ({ id: dep.id, name: dep.name }));
-      return departamentosConId;
+      const response = await axios.get('http://127.0.0.1:8000/api/departamentos');
+      const data = response.data.departamentos;
+      
+      // Verifica que data sea un array antes de usar map
+      if (Array.isArray(data)) {
+        const departamentosConId = data.map((dep) => ({ id: dep.id, Departamento: dep.Departamento }));
+        return departamentosConId;
+      } else {
+        console.error("El servidor no devolvió un array de departamentos:", data);
+        throw new Error("Respuesta del servidor no válida");
+      }
     } catch (error) {
       console.error("Error al cargar departamentos:", error);
       throw error;
