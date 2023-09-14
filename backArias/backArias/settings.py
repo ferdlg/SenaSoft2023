3235bd5fc0533095d7d7251e811522c503a874df
Mafe <ferdlgpar@gmail.com>
2023-09-14 11:44:01 -0500

    url-filtro

--- backArias/backArias/settings.py
+++ backArias/backArias/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'nexos_db',
-        'PASSWORD': '666666',
+        'PASSWORD': '1234567890',
         'USER': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
