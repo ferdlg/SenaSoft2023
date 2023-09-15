20afdc6e57f8f1e4d20685136fcf68917d714cbc
Mafe <ferdlgpar@gmail.com>
2023-09-15 00:10:38 -0500

    comentario

--- Back/api/urls.py
+++ Back/api/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns=[
     path('empleados/', EmpleadosView.as_view(), name='empleados_list'),
     path('empleados/<int:id>/', EmpleadosView.as_view(), name='empleados_process'), #url para un unico empleado 
-        path('empleadoUpdate/<int:id>/', EmpleadosView.as_view(), name='empleados_process'), #url para un unico empleado 
+        path('empleadoUpdate/<int:id>/', EmpleadosView.as_view(), name='empleados_process'), #url para un unico empleado actualizacion
 
     path('departamentos/', obtener_departamentos, name='obtener_departamentos'),
     path('ciudades/', obtener_ciudades_departamento, name='obtener_ciudades_departamento'),
