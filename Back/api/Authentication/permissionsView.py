17160d5dc2c94d43c72485bdc6ca45fa380c3603
Mafe <ferdlgpar@gmail.com>
2023-09-15 16:26:18 -0500

    migraciones-permisos

--- Back/api/Authentication/permissionsView.py
+++ Back/api/Authentication/permissionsView.py
@@ -1,9 +1,15 @@
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
-from api.models import Empleados, Ciudades, Departamentos
+from django.db.models.signals import post_migrate
+from django.contrib.auth.management import create_permissions
+from api.models import Permissions
+from django.apps import apps
 
-content_type = ContentType.objects.get_for_model(Empleados)
-
-permission = Permission.objects.create(
-    
-)
+def create_permissions(sender, **kwargs):
+        app_models = apps.get_app_config(sender.label).get_models()
+        for model in app_models:
+                Permissions.objects.get_or_create(
+                        codename = f' can_custom_permission_{model._meta.mode_name}',
+                        name = f'Can custom permission for {model._meta.verbose_name}',
+                        content_type = None,
+                )
+                
+post_migrate.connect(create_permissions)
--- Back/api/models.py
+++ Back/api/models.py
@@ -58,3 +58,12 @@ class Roles(models.Model):
     class Meta:
         db_table = 'roles'
 
+class Permissions(models.Model):
+    nombre_permiso = models.CharField(max_length=300)
+    codename = models.CharField(max_length=200, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'permissions'
