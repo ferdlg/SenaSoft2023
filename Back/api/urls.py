e2f5717fce4afe9ee4b9c68d07a45a1cb4463de2
Mafe <ferdlgpar@gmail.com>
2023-09-15 11:15:29 -0500

    correcciones

--- Back/api/urls.py
+++ Back/api/urls.py
@@ -3,6 +3,7 @@ from api.Controllers.empleadosView import EmpleadosView
 from api.Controllers.departamentoView import obtener_departamentos
 from api.Controllers.ciudadesView import obtener_ciudades_departamento
 from api.Controllers.filtroCiudadesView import filtro_ciudades
+from api.Controllers.tipoDocumentoView import mostrar_tipo_documento
 
 urlpatterns=[
     path('empleados/', EmpleadosView.as_view(), name='empleados_list'),
@@ -12,5 +13,5 @@ urlpatterns=[
     path('departamentos/', obtener_departamentos, name='obtener_departamentos'),
     path('ciudades/', obtener_ciudades_departamento, name='obtener_ciudades_departamento'),
     path('filtro/<int:id_departamento>/', filtro_ciudades, name='filtro_ciudades'),
-
+    path('Tipo_documento/', mostrar_tipo_documento, name='Tipo_documentos_mostrar'),
 ]
--- Back/models.py
+++ Back/models.py
@@ -6,94 +6,13 @@
 #   * Remove ` ` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-         
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-         
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-         
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-         
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-         
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-         
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class AuthtokenToken(models.Model):
-    key = models.CharField(primary_key=True, max_length=40)
-    created = models.DateTimeField()
-    user = models.OneToOneField(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-         
-        db_table = 'authtoken_token'
-
-
 class Ciudades(models.Model):
     id_ciudad = models.IntegerField(primary_key=True)
     id_departamento_fk = models.ForeignKey('Departamentos', models.DO_NOTHING, db_column='id_departamento_fk')
     nombre_ciudad = models.CharField(max_length=30, blank=True, null=True)
 
     class Meta:
-         
+        
         db_table = 'ciudades'
 
 
@@ -101,58 +20,12 @@ class Departamentos(models.Model):
     id_departamento = models.IntegerField(primary_key=True)
     codigo_departamento = models.IntegerField(blank=True, null=True)
     nombre_departamento = models.CharField(max_length=30, blank=True, null=True)
-    fecha_hora_crear = models.DateTimeField()
-    fecha_hora_modificar = models.DateTimeField()
+    fecha_hora_crear = models.DateTimeField(auto_now_add= True)
+    fecha_hora_modificar = models.DateTimeField(auto_now= True)
 
     class Meta:
-         
+        
         db_table = 'departamentos'
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-         
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-         
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-         
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-         
-        db_table = 'django_session'
-
 
 class Empleados(models.Model):
     id_empleado = models.AutoField(primary_key=True)
@@ -165,11 +38,11 @@ class Empleados(models.Model):
     direccion = models.CharField(max_length=50, blank=True, null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
     telefono = models.IntegerField(blank=True, null=True)
-    fecha_hora_crear = models.DateTimeField()
-    fecha_hora_modificar = models.DateTimeField()
+    fecha_hora_crear = models.DateTimeField(auto_now_add= True)
+    fecha_hora_modificar = models.DateTimeField(auto_now= True)
 
     class Meta:
-         
+        
         db_table = 'empleados'
 
 
@@ -178,5 +51,6 @@ class TipoDocumento(models.Model):
     nombre_tipo_documento = models.CharField(max_length=10)
 
     class Meta:
-         
+        
         db_table = 'tipo_documento'
+
