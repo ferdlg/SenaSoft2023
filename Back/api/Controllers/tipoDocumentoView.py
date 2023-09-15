6644aa2e572d91c4697c79d570bcd03f6038c134
Mafe <ferdlgpar@gmail.com>
2023-09-15 11:31:19 -0500

    mostrar_tipo_documento

--- Back/api/Controllers/tipoDocumentoView.py
+++ Back/api/Controllers/tipoDocumentoView.py
@@ -5,7 +5,7 @@ from api.models import TipoDocumento
 from api.serializers import TipoDocumentoSerializer
 import json
 
-def mostrar_tipo_documento(self, request):
+def mostrar_tipo_documento(request):
     tipo_documento = TipoDocumento.objects.all()
     serializer = TipoDocumentoSerializer(tipo_documento, many=True)
 
