b7ff50b1216b263b49e41e6d537ed5f8ca455303
Mafe <ferdlgpar@gmail.com>
2023-09-15 11:20:05 -0500

    modelo-tipodocumento

---
+++ Back/api/Controllers/tipoDocumentoView.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from api.models import TipoDocumento
+from serializers import TipoDocumentoSerializer
+import json
+
+def mostrar_tipo_documento(self, request):
+    tipo_documento = TipoDocumento.objects.all()
+    serializer = TipoDocumentoSerializer(tipo_documento, many=True)
+
+    if len(tipo_documento)>0:
+            datos = {'message':"Succes", 'Tipo de documentos':serializer.data}
+    else: 
+            datos = {'message':"No se encontraron tipos de documentos..."}
+    return JsonResponse(datos)
